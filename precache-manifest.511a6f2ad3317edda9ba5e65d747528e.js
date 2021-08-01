af281740159cccce6437eaf614edc97a80f6d400
deaoo <deaoosh@gmail.com>
2021-08-01 12:30:53 +0200

    Add files via upload

---
+++ precache-manifest.511a6f2ad3317edda9ba5e65d747528e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7067b5baf906caea9958",
+    "url": "/css/about.6006b5d0.css"
+  },
+  {
+    "revision": "f3cd9210c502b81bc819",
+    "url": "/css/app.82caace5.css"
+  },
+  {
+    "revision": "6dd05171ec5500c022a23ccaabba09ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7067b5baf906caea9958",
+    "url": "/js/about.d4af0870.js"
+  },
+  {
+    "revision": "f3cd9210c502b81bc819",
+    "url": "/js/app.dbc104fd.js"
+  },
+  {
+    "revision": "9a645445c07ba616f181",
+    "url": "/js/chunk-vendors.3eaed59f.js"
+  },
+  {
+    "revision": "934e8bfa809723289db50225f60d0184",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
